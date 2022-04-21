e8654c1ae0e157196a70b6533daa513da5738545
sunami09 <sunamidasgupta@gmail.com>
2022-04-21 17:34:45 +0530

    Favicon Added

--- .next/static/webpack/webpack.e1495112bbc2391c.hot-update.js
+++ .next/static/webpack/webpack.e1495112bbc2391c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c0cec3344fb31907"; }
+/******/ 	__webpack_require__.h = function() { return "d1b693f2baaf2ec9"; }
 /******/ }();
 /******/ 
 /******/ }
