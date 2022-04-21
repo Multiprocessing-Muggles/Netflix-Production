d2ef11de181045e3d1f3cd8d61606e8708a9d932
sunami09 <sunamidasgupta@gmail.com>
2022-04-21 16:58:33 +0530

    Netflix Version 1.0.1 - Complete

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# Netflix-Production
+<div align="center" >
+  
+  <h1> 🔴 Netflix </h1>
+  <img src="https://user-images.githubusercontent.com/66564001/163689958-09b30d37-a5d8-4058-b695-73cbccb3cb20.gif" alt="Welcome!" width="450"/>
+  <hr>
+</div>
+
+# [🟢 Live Sever](https://netflix-clone-sunami.netlify.app/)
+
+<br>
+<hr>
+
+# Demo
+
+## Header Section - Completed
+
+https://user-images.githubusercontent.com/66564001/163690298-1be8c8b0-ac77-4f26-814f-72cd71e7fb8f.mp4
+
+
+## Banner Section - Completed
+
+* Used TMDB api to fetch the movie data and the displayed it.
+
+
+https://user-images.githubusercontent.com/66564001/163800821-13e573e6-98cb-4b01-91f9-ccc3e4183a30.mp4
+
+## Row Section - Completed
+
+* HomePage UI - Completed
+
+https://user-images.githubusercontent.com/66564001/163980517-06d29371-a87a-41f2-8c01-995bdad83a56.mp4
+
+
+https://user-images.githubusercontent.com/66564001/163980959-c6728fdd-13c7-4a7e-9345-91818cce3fda.mp4
+
+## Login PageUI - Completed
+
+
+https://user-images.githubusercontent.com/66564001/163990923-08d73943-b844-4809-99e7-8b8330d1d874.mp4
+
+
+## Login Page Functionality - Completed
+
+* Loging in with existing account
+
+https://user-images.githubusercontent.com/66564001/164234432-87930f41-45a8-471a-93a9-df13e4f9eeb3.mp4
+
+
+* Trying to LogIn with a non-existing account
+
+https://user-images.githubusercontent.com/66564001/164234700-4c8c259e-321e-442c-bda1-e0c05e513e7f.mp4
+
+<!-- For Soham * SignUp with a new account
+
+
+https://user-images.githubusercontent.com/66564001/164236038-2e93e69c-7953-4ba2-b976-6740e71d0c78.mp4
+
+
+
+
+https://user-images.githubusercontent.com/66564001/164236351-30492432-42a8-4166-9395-8c496892c1b4.mp4 -->
+
+
+
+
+
---
+++ components/Banner.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image"
+import { useEffect, useState } from 'react';
+import { baseUrl } from "../constants/movie";
+import { Movie } from "../typings"
+import { FaPlay } from "react-icons/fa"
+import { InformationCircleIcon } from "@heroicons/react/solid";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
+
+
+interface Props {
+    netflixOriginals : Movie[]
+}
+
+function Banner( { netflixOriginals }: Props) {
+
+    const [movie, setMovie] = useState<Movie | null>(null)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+  const [showModal, setShowModal] = useRecoilState(modalState)
+
+    useEffect(() => {
+        //Bug Fixed
+        
+        let k = Math.floor(Math.random() * netflixOriginals.length)
+        while(
+               netflixOriginals[k].original_name == "Pil's Adventures" || netflixOriginals[k].title == "Pil's Adventures" || netflixOriginals[k].name == "Pil's Adventures"
+            || netflixOriginals[k].original_name == "Gold" || netflixOriginals[k].title == "Gold" || netflixOriginals[k].name == "Gold"
+            || netflixOriginals[k].original_name == "Encanto" || netflixOriginals[k].title == "Encanto" || netflixOriginals[k].name == "Encanto"
+            || netflixOriginals[k].original_name == "My Hero Academia: World Heroes' Mission" || netflixOriginals[k].title == "My Hero Academia: World Heroes' Mission" || netflixOriginals[k].name == "My Hero Academia: World Heroes' Mission"
+        )
+        {
+            k = Math.floor(Math.random() * netflixOriginals.length)
+        }
+        console.log(k)
+        setMovie(netflixOriginals[k])
+    },[netflixOriginals])
+
+    return (
+        <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+
+            <div className="absolute top-0 left-0 h-[95vh] w-screen -z-10 pt-10">
+                <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} 
+                layout="fill"
+                objectFit="cover"
+                />
+            </div>
+
+            <h1 className="text-2xl font-bold md:txet-4xl lg:text-5xl">{movie?.title || movie?.name || movie?.original_name}</h1>
+
+            <p className="max-w-xs text-shadow-md text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
+
+            <div className="flex space-x-3">
+                <button className="bannerButton bg-white text-black" onClick={() => {
+            setCurrentMovie(movie)
+            setShowModal(true)
+          }}>
+                    <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7"/>
+                    Play</button>
+                <button className="bannerButton bg-[gray]/70" onClick={() => {
+            setCurrentMovie(movie)
+            setShowModal(true)
+          }}>More Info <InformationCircleIcon 
+                className="h-5 w-5 md:h-8 md:w-8" /></button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Banner
